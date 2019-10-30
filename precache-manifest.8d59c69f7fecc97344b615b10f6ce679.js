22ce4317e40dbe38d70c33c4d098124fac01ce22
hmnts999 <hmnts999@gmail.com>
2019-10-31 01:16:24 +0530

    Updates

---
+++ precache-manifest.8d59c69f7fecc97344b615b10f6ce679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2382eea25ca5210c5460b315fcf6a469",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "c2c4b24fe44c56efedd5",
+    "url": "/react-cards/static/css/main.90944168.chunk.css"
+  },
+  {
+    "revision": "08adf9482cc7baf74e94",
+    "url": "/react-cards/static/js/2.5e9077e2.chunk.js"
+  },
+  {
+    "revision": "c2c4b24fe44c56efedd5",
+    "url": "/react-cards/static/js/main.6afacede.chunk.js"
+  },
+  {
+    "revision": "6b404ae3aa27562b0531",
+    "url": "/react-cards/static/js/runtime-main.73365585.js"
+  }
+]);
